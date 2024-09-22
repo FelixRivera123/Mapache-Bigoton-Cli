ab8b58324d9ccfbfa347b1a4ddfccb3e7c61bbf5
alex2227 <alex2227@hotmail.com>
2024-09-22 01:14:42 -0600

    Establecida Home y arquitectura inicial de la pagina y archivos --REV --Rev => Logo no funciona

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import Gallery from "./Components/Gallery/Gallery";
+import Services from "./Components/Home/Services";
+import Schedule from "./Components/Home/Citas";
+import {BrowserRouter} from "react-router-dom";
 
 const App = () => {
     return (
-        <div className={'card'}>
-            <h1 className={'text-center text-black'}>
-                Hola Mundo
-            </h1>
-        </div>
-    )
+        <BrowserRouter>
+            <Header/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
